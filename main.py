5aae30ad867bd1ec005c1c2682e71674629dfb10
Chris Wood <chris_wood@bcit.ca>
2019-04-01 13:42:07 -0700

    added function to handle lookupPlate intent from dialogflow

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 from flask import Flask, request, make_response, jsonify
 from loguru import logger
 import requests
+from bs4 import BeautifulSoup
+import re
 import json # maybe not needed
 
 app = Flask(__name__)
@@ -17,26 +19,56 @@ def webhook():
     except AttributeError:
         return 'json error'
 
-    logger.debug("Intent: {}".format(intent))
-    logger.debug("Full Request: \n {}".format(req))
+    logger.debug("Intent: {}", intent)
+    logger.debug("Full Request: \n {}", req)
 
-    # if action == 'weather':
-    #     res = weather(req)
-    # elif action == 'weather.activity':
-    #     res = weather_activity(req)
-    # elif action == 'weather.condition':
+    if intent == 'testIntent':
+        res = 'testIntent'
+    elif intent == 'lookupPlate':
+        res = scrape(req.get('queryResult').get('parameters').get('any'))
+    # elif intent == 'weather.condition':
     #     res = weather_condition(req)
-    # elif action == 'weather.outfit':
+    # elif intent == 'weather.outfit':
     #     res = weather_outfit(req)
-    # elif action == 'weather.temperature':
+    # elif intent == 'weather.temperature':
     #     res = weather_temperature(req)
-    # else:
-    #     log.error('Unexpected action.')
+    else:
+        logger.error('Unexpected action.')
 
     # print('Action: ' + action)
     # print('Response: ' + res)
 
-    return make_response(jsonify({'fulfillmentText': "intent {}".format(intent)}))
+    return make_response(jsonify({'fulfillmentText': res}))
+
+
+@app.route('/yeet', methods=['GET'])
+def test():
+    return scrape('LOVEYY')
+
+def scrape(plate):
+    if re.match("^[a-zA-Z0-9]{2,6}$", plate) is None:
+        return "Please request a valid plate number. (Min: 2, Max: 6)"
+
+    cookies_url = "https://payments.impark.com"
+    url = "https://payments.impark.com/Notice/?lp={}&ps=BC".format(plate)
+
+    headers = {'Host': 'payments.impark.com', 'Connection': 'keep-alive', 'Cache-Control': 'max-age=0', 'Upgrade-Insecure-Requests': '1', 'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/65.0.3325.181 Safari/537.36', 'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8', 'Referer': 'https://payments.impark.com/Home/Search?q=license', 'Accept-Encoding': 'gzip, deflate, br', 'Accept-Language': 'en-US,en;q=0.9'}
+    cookies_req = requests.post(cookies_url)
+
+    resp = requests.get(url, headers=headers, cookies=cookies_req.cookies)
+    html = BeautifulSoup(resp.content, 'html.parser')
+
+    rst = html.select("td.noticeNum")
+
+    ticket_amounts = html.select("td.amount")
+    total = 0
+    for t in ticket_amounts:
+        total += float(t.text.strip().replace('$',''))
+
+    if len(rst) == 0:
+        return "There are no parking tickets for {}.".format(plate)
+    else:
+        return "{} has {} parking tickets owing {}".format(plate, len(rst), '${:,.2f}'.format(total))
 
 if __name__ == "__main__":
     app.run()
